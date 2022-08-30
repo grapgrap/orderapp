983a66c3a7aee83f17a2f76b58e572fe4a3d1908
YouYou94 <ybh4115@gmail.com>
2022-08-30 14:23:03 +0900

    Request 엎고 다시 구현

--- src/Components/OrderPage/OrderPage.jsx
+++ src/Components/OrderPage/OrderPage.jsx
@@ -1,9 +1,6 @@
 import styled from 'styled-components';
-import Payment from './Payment';
 import Requests from './Requests/Requests';
 import UserInfo from './UserInfo/UserInfo';
-import Discount from './Discount';
-import OrderInfo from './OrderInfo';
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
@@ -36,7 +33,6 @@ function OrderPage({ userInfo, setUserInfo }) {
 
   // 주문 요청 사항 state
   const [orderRequest, setOrderRequest] = useState('벨은 누르지 말아주세요!');
-  const [additionalRequest, setAdditionalRequest] = useState(option);
   const [isCustom, setIsCustom] = useState(false);
 
   const onOrderCheck = () => {
@@ -44,34 +40,13 @@ function OrderPage({ userInfo, setUserInfo }) {
       `주소: ${userInfo.address} ${userInfo.detail}\n전화번호: ${userInfo.phoneNum}\n주문 요청 사항: ${orderRequest}`
     );
 
-    if (isCustom === true) {
-      if (additionalRequest.length < 3) {
-        setAdditionalRequest([
-          ...additionalRequest,
-          {
-            key: option.length - 1,
-            value: option.length - 1,
-            label: orderRequest,
-          },
-        ]);
-      }
-    }
     //userNav('/orderapp/');
   };
 
   return (
     <OrderPageWrap>
       <UserInfo userInfo={userInfo} setUserInfo={setUserInfo} />
-      <Requests
-        setOrderRequest={setOrderRequest}
-        isCustom={isCustom}
-        setIsCustom={setIsCustom}
-        option={option}
-        additionalRequest={additionalRequest}
-      />
-      <Payment />
-      <Discount />
-      <OrderInfo />
+      <Requests />
       <OrderBtn onClick={onOrderCheck}>결제하기</OrderBtn>
     </OrderPageWrap>
   );
--- src/Components/OrderPage/Requests/Requests.jsx
+++ src/Components/OrderPage/Requests/Requests.jsx
@@ -2,64 +2,8 @@ import { useState } from 'react';
 
 import * as Styled from './Styled.jsx';
 
-const CUSTOM_VALUE = 'CUSTOM';
-
-function Requests({
-  additionalRequest,
-  option,
-  isCustom,
-  setIsCustom,
-  setOrderRequest,
-}) {
-  // 주문 요청 사항
-  const optionList = [];
-  optionList.push(
-    additionalRequest.map(op => {
-      return (
-        <Styled.RequestsSelectOption key={op.key} value={op.value}>
-          {op.label}
-        </Styled.RequestsSelectOption>
-      );
-    })
-  );
-  console.log(additionalRequest);
-
-  const [customLength, setCustomLength] = useState(0);
-  const onChangeRequest = event => {
-    if (event.target.value === CUSTOM_VALUE) {
-      setIsCustom(true);
-      setOrderRequest('');
-    } else {
-      setOrderRequest(event.target[event.target.value].innerText);
-      setIsCustom(false);
-    }
-  };
-
-  const onChangeCustom = event => {
-    if (event.target.value.length <= 60) {
-      setOrderRequest(event.target.value);
-      setCustomLength(event.target.value.length);
-    } else if (event.target.value.length > 60) {
-      event.target.value = event.target.value.slice(0, -1);
-    }
-  };
-
-  return (
-    <Styled.ReuestsWrap>
-      <Styled.ReuestsTitle>주문 요청 사항</Styled.ReuestsTitle>
-      <Styled.RequestsSelect onChange={onChangeRequest}>
-        {optionList}
-      </Styled.RequestsSelect>
-      {isCustom ? (
-        <>
-          <Styled.CustomTextArea onChange={onChangeCustom} />
-          <Styled.CustomTextNum>{customLength}/60</Styled.CustomTextNum>
-        </>
-      ) : (
-        <></>
-      )}
-    </Styled.ReuestsWrap>
-  );
+function Requests({}) {
+  return;
 }
 
 export default Requests;
