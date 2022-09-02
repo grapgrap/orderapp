b6fe86a394ddf91c3c423a1c987e40f3f0b48433
YouYou94 <ybh4115@gmail.com>
2022-09-02 19:42:03 +0900

    테스트

--- src/Components/OrderPage/OrderPage.jsx
+++ src/Components/OrderPage/OrderPage.jsx
@@ -81,7 +81,7 @@ function OrderPage({ user, setUser }) {
   // 주문 내역
   const menuList = [
     { id: 0, name: 'BBQ 황금올리브', price: 18000 },
-    { id: 1, name: '맘스터치 싸이버거', price: 5400 },
+    { id: 1, name: '태백산 감자버거', price: 5400 },
     { id: 2, name: '장충동 왕족발', price: 32000 },
   ];
 
