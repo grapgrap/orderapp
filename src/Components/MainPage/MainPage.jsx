db89e5fd2cabbd3dc965a2425f89dcceaf9526c9
YouYou94 <ybh4115@gmail.com>
2022-09-02 14:26:46 +0900

    메뉴 데이터 수정

--- src/Components/MainPage/MainPage.jsx
+++ src/Components/MainPage/MainPage.jsx
@@ -24,32 +24,16 @@ function MainPage({ menu, setMenu }) {
 
   return (
     <Styled.MainPageWrap>
-      <Styled.OrderMenu>
-        <Styled.OrderMenuCheck type="checkbox" name="menu01" />
-        <Styled.OrderMenuLabel htmlFor="menu01">
-          BBQ 황금올리브
-        </Styled.OrderMenuLabel>
-      </Styled.OrderMenu>
-      <Styled.OrderMenu>
-        <Styled.OrderMenuCheck type="checkbox" name="menu02" />
-        <Styled.OrderMenuLabel htmlFor="menu02">
-          맘스터치 싸이버거
-        </Styled.OrderMenuLabel>
-      </Styled.OrderMenu>
-      <Styled.OrderMenu>
-        <Styled.OrderMenuCheck type="checkbox" name="menu02" />
-        <Styled.OrderMenuLabel htmlFor="menu02">
-          도미노피자 포테이토 피자
-        </Styled.OrderMenuLabel>
-      </Styled.OrderMenu>
-      {/* 버튼 선택 시 메뉴 setting */}
+      <Styled.OrderMenu></Styled.OrderMenu>
       <Styled.OrderBtn
         onClick={() => {
-          if (!menu) {
-            alert('메뉴를 선택해주세요!');
-          } else {
-            userNav('/orderapp/order');
-          }
+          // if (!menu) {
+          //   alert('메뉴를 선택해주세요!');
+          // } else {
+          //   userNav('/orderapp/order');
+          // }
+
+          userNav('/orderapp/order');
         }}
       >
         주문하기
