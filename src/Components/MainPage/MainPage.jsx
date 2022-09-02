ee95c605f1da94e5de67d262309df574c3246b6a
YouYou94 <ybh4115@gmail.com>
2022-09-02 11:08:44 +0900

    메뉴 데이터 추가

--- src/Components/MainPage/MainPage.jsx
+++ src/Components/MainPage/MainPage.jsx
@@ -3,6 +3,24 @@ import { useNavigate } from 'react-router-dom';
 
 function MainPage({ menu, setMenu }) {
   let userNav = useNavigate();
+  const menuList = {
+    menu01: {
+      name: 'BBQ 황금올리브',
+      price: 18000,
+    },
+    menu02: {
+      name: '맘스터치 싸이버거',
+      price: 5400,
+    },
+    menu03: {
+      name: '도미노 포테이토피자',
+      price: 12000,
+    },
+    menu04: {
+      name: '캐비어',
+      price: 1000000,
+    },
+  };
 
   return (
     <Styled.MainPageWrap>
