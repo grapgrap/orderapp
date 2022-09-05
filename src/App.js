0a617b4253084f771423fccc60fef5e7b812b696
YouYou94 <ybh4115@gmail.com>
2022-09-05 11:50:07 +0900

    OrderPage Styled-Components 구현

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainPage from './Components/MainPage/MainPage';
+import OrderPage from './Components/OrderPage/OrderPage';
 // import axios from 'axios';
 
 function App() {
@@ -11,7 +12,7 @@ function App() {
           {/* 메인 페이지 라우터 */}
           <Route path="/orderapp" element={<MainPage />} />
           {/* 주문 페이지 라우터 */}
-          <Route />
+          <Route path="/orderapp/order" element={<OrderPage />} />
         </Routes>
       </BrowserRouter>
     </div>
---
+++ src/Components/OrderPage/OrderPage.jsx
@@ -0,0 +1,8 @@
+import * as Styled from './Styled.jsx';
+import * as CommonStyled from '../Common/Styled.jsx';
+
+function OrderPage() {
+  return <CommonStyled.PageWrap></CommonStyled.PageWrap>;
+}
+
+export default OrderPage;
