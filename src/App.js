e864f475247bc180f28a846ef02ed6f453415da7
YouYou94 <ybh4115@gmail.com>
2022-09-06 19:08:46 +0900

    사용자 로딩화면 구현

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { useState } from 'react';
+import Loading from './Components/Loading';
 
 function App() {
+  const [user, setUser] = useState('');
+  const [coupon, setCoupon] = useState('');
+  const [isLoading, setLoading] = useState(false);
+
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          {/* 메인 페이지 라우터 */}
-          <Route path="/orderapp" />
-          {/* 주문 페이지 라우터 */}
-          <Route path="/orderapp/order" />
+          {isLoading ? (
+            <>
+              {/* 메인 페이지 라우터 */}
+              <Route path="/orderapp" />
+              {/* 주문 페이지 라우터 */}
+              <Route path="/orderapp/order" />
+            </>
+          ) : (
+            <>
+              {/* 사용자 로딩 화면 */}
+              <Route path="/orderapp" element={<Loading />} />
+            </>
+          )}
         </Routes>
       </BrowserRouter>
     </div>
---
+++ src/Components/Loading.jsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+function Loading() {
+  return <LoadingScreen>잠시만 기다려주세요...</LoadingScreen>;
+}
+
+export default Loading;
+
+export const LoadingScreen = styled.div``;
