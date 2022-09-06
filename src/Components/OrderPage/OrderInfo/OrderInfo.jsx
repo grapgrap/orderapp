d94d1de438f9dac2ac07fc1637a239e1351e51d1
YouYou94 <ybh4115@gmail.com>
2022-09-06 14:11:49 +0900

    OrderInfo, Styled-Components 추가 및 배달 주문 내역 구현

--- src/Components/OrderPage/OrderInfo/OrderInfo.jsx
+++ src/Components/OrderPage/OrderInfo/OrderInfo.jsx
@@ -2,11 +2,48 @@ import * as Styled from './Styled.jsx';
 import * as CommonStyled from '../../Common/Styled.jsx';
 
 function OrderInfo({ orderList }) {
+  const orderHistory = [];
+  let resultPrice = 0;
+
+  for (let i = 0; i < orderList.length; i++) {
+    orderHistory.push(
+      <Styled.OrderHistoryContent key={orderList[i].id}>
+        <Styled.OrderHistoryContentSpan>
+          {orderList[i].name}
+        </Styled.OrderHistoryContentSpan>
+        <Styled.OrderHistoryContentSpan>
+          {orderList[i].price
+            .toString()
+            .replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ',')}
+          원
+        </Styled.OrderHistoryContentSpan>
+      </Styled.OrderHistoryContent>
+    );
+    resultPrice = resultPrice + orderList[i].price;
+    if (i === orderList.length - 1) {
+      orderHistory.push(
+        <Styled.OrderHistoryContent key="Result">
+          <Styled.OrderHistoryContentSpan>
+            총 결제 금액
+          </Styled.OrderHistoryContentSpan>
+          <Styled.OrderHistoryContentSpan>
+            {resultPrice
+              .toString()
+              .replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ',')}
+            원
+          </Styled.OrderHistoryContentSpan>
+        </Styled.OrderHistoryContent>
+      );
+    }
+  }
+  // if (i === orderList.length - 1) {
+
   return (
     <CommonStyled.OrderPageSection>
       <CommonStyled.OrderPageSectionTitle>
-        주문 내역
+        배달 주문 내역
       </CommonStyled.OrderPageSectionTitle>
+      <Styled.OrderHistorySection>{orderHistory}</Styled.OrderHistorySection>
     </CommonStyled.OrderPageSection>
   );
 }
