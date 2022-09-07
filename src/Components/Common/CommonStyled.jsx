f877b3e438fdb121f4167d229fdf2f79fc58bfb3
YouYou94 <ybh4115@gmail.com>
2022-09-07 11:20:57 +0900

    Common Styled-Components 추가

---
+++ src/Components/Common/CommonStyled.jsx
@@ -0,0 +1,25 @@
+import styled from 'styled-components';
+
+export const PageWrap = styled.div`
+  min-width: 20rem;
+  max-width: 30rem;
+  height: 100%;
+  background-color: rgb(250, 250, 236);
+  display: flex;
+  flex-direction: column;
+`;
+
+export const PageButton = styled.button`
+  height: 2rem;
+  margin-bottom: 2rem;
+  border-radius: 2rem;
+  border: none;
+  background: rgb(134, 134, 252);
+  color: white;
+  font-size: 1rem;
+  cursor: pointer;
+
+  :hover {
+    background: rgb(252, 134, 189);
+  }
+`;
