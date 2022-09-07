12df18fa56e0a3491ba3dc58b288b621fc6310d6
YouYou94 <ybh4115@gmail.com>
2022-09-07 18:02:30 +0900

    Payment, Styled-Components 추가

---
+++ src/Components/OrderPage/Payment/Styled.jsx
@@ -0,0 +1,33 @@
+import styled from 'styled-components';
+
+export const PaymentSlide = styled.div`
+  width: 100%;
+  height: 10rem;
+  overflow: scroll;
+`;
+
+export const PaymentCard = styled.div`
+  width: 100%;
+  height: 9rem;
+  background: rgb(128, 128, 128, 0.5);
+  white-space: nowrap;
+  border-radius: 1rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const VendorNameSpan = styled.span`
+  margin-top: 0.5rem;
+`;
+
+export const CardNumberSpan = styled.span`
+  flex: 1;
+  text-align: center;
+  padding-top: 2rem;
+`;
+
+export const PaymentCardBtn = styled.button`
+  margin-bottom: 0.5rem;
+  width: 10rem;
+`;
