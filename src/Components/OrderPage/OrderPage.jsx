410a78e43f08b624c7eecc5c16fa346b4a548016
YouYou94 <ybh4115@gmail.com>
2022-09-07 14:20:23 +0900

    OrderPage, 주문요청사항 기본 옵션 하드 코딩 및 스테이트 추가

--- src/Components/OrderPage/OrderPage.jsx
+++ src/Components/OrderPage/OrderPage.jsx
@@ -1,8 +1,38 @@
+import { useState } from 'react';
 import * as CommonStyled from '../Common/CommonStyled.jsx';
 import Request from './Request/Request.jsx';
 import UserInfo from './UserInfo/UserInfo.jsx';
 
 function OrderPage({ user, setUser }) {
+  // 주문 요청 사항
+  // 기본 주문 요청 사항 리스트
+  const requestOption = [
+    {
+      id: 0,
+      value: 0,
+      label: '벨은 누르지 말아주세요!',
+    },
+    {
+      id: 1,
+      value: 1,
+      label: '문 앞에 놓아 주시고 연락 주세요.',
+    },
+    {
+      id: 2,
+      value: 2,
+      label: '반찬은 안 주셔도 되요.',
+    },
+    {
+      id: 'CUSTOM',
+      value: 'CUSTOM',
+      label: '직접 입력',
+    },
+  ];
+  // 결제 시 주문 요청 사항
+  const [orderRequest, setOrderRequest] = useState('벨은 누르지 말아주세요!');
+  // 직접 입력인지 아닌지
+  const [isCustom, setIsCustom] = useState(false);
+
   // 결제하기
   const onOrderCheck = () => {
     alert(
