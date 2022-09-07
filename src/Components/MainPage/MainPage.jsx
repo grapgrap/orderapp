4a42f5ab7dd753ea98634f1953d93aed02cd3bc6
YouYou94 <ybh4115@gmail.com>
2022-09-07 11:21:38 +0900

    MainPage, Styled-Components 구현 및 버튼 클릭 시 주문 페이지 이동 기능 구현

--- src/Components/MainPage/MainPage.jsx
+++ src/Components/MainPage/MainPage.jsx
@@ -1,10 +1,23 @@
+import * as CommonStyled from '../Common/CommonStyled.jsx';
+import styled from 'styled-components';
+import { useNavigate } from 'react-router-dom';
+
 function MainPage({ user }) {
+  let userNav = useNavigate();
+  const goOrder = () => userNav('/orderapp/order');
+
   return (
-    <div>
-      메인페이지입니다.
-      {user.coupons.length}
-    </div>
+    <CommonStyled.PageWrap>
+      <MainPageDiv></MainPageDiv>
+      <CommonStyled.PageButton onClick={goOrder}>
+        주문하기
+      </CommonStyled.PageButton>
+    </CommonStyled.PageWrap>
   );
 }
 
 export default MainPage;
+
+export const MainPageDiv = styled.div`
+  flex: 1;
+`;
