3f4b2602e04e38b49d4964e2b99a1b0186d4b0f7
YouYou94 <ybh4115@gmail.com>
2022-09-08 17:40:01 +0900

    CommonStyled, Styled-Components 수정

--- src/Components/Common/CommonStyled.jsx
+++ src/Components/Common/CommonStyled.jsx
@@ -25,7 +25,7 @@ export const PageButton = styled.button`
 `;
 
 export const OrderPageSection = styled.div`
-  margin-bottom: 1rem;
+  margin-bottom: 0.5rem;
   display: flex;
   flex-direction: column;
 `;
