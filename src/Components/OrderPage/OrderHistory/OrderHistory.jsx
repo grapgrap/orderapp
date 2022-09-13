0dc89f7cfa0582d855527a98e01683b102d1ebd5
YouYou94 <ybh4115@gmail.com>
2022-09-13 18:08:06 +0900

    OrderHistory, 장바구니 display

--- src/Components/OrderPage/OrderHistory/OrderHistory.jsx
+++ src/Components/OrderPage/OrderHistory/OrderHistory.jsx
@@ -1,12 +1,40 @@
 import * as CommonStyled from '../../Common/CommonStyled.jsx';
 import * as Styled from './Styled.jsx';
 
-function OrderHistory() {
+function OrderHistory({ orderList, result, setResult }) {
+  let totalPrice = 0;
+
+  // 장바구니
+  const orderBasket = orderList.map(order => {
+    totalPrice = totalPrice + order.price;
+    return (
+      <Styled.OrderHistoryContent key={order.id}>
+        <Styled.OrderHistoryContentSpan>
+          {order.name}
+        </Styled.OrderHistoryContentSpan>
+        <Styled.OrderHistoryContentSpan>
+          {order.price} 원
+        </Styled.OrderHistoryContentSpan>
+      </Styled.OrderHistoryContent>
+    );
+  });
+
   return (
     <CommonStyled.OrderPageSection>
       <CommonStyled.OrderPageSectionTitle>
         배달 주문 내역
       </CommonStyled.OrderPageSectionTitle>
+      <Styled.OrderHistorySection>
+        {orderBasket}
+        <Styled.OrderHistoryContent key="RESULT">
+          <Styled.OrderHistoryContentSpan>
+            총 결제 금액
+          </Styled.OrderHistoryContentSpan>
+          <Styled.OrderHistoryContentSpan>
+            {totalPrice} 원
+          </Styled.OrderHistoryContentSpan>
+        </Styled.OrderHistoryContent>
+      </Styled.OrderHistorySection>
     </CommonStyled.OrderPageSection>
   );
 }
