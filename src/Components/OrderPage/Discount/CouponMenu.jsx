34c5bee2fdda9dcc4a021fd553f2df59423110bb
YouYou94 <ybh4115@gmail.com>
2022-09-13 17:40:20 +0900

    Discount, 쿠폰 사용 기능 구현

--- src/Components/OrderPage/Discount/CouponMenu.jsx
+++ src/Components/OrderPage/Discount/CouponMenu.jsx
@@ -1,26 +1,49 @@
 import * as Styled from './Styled.jsx';
 import * as CommonStyled from '../../Common/CommonStyled.jsx';
-import { FIXED, RATED } from '../../../Constants.js';
+import { COUPON, FIXED, RATED } from '../../../Constants.js';
 
-function CouponMenu({ coupon }) {
+function CouponMenu({ coupon, setResult, setIsMenu }) {
+  const onUseCoupon = event => {
+    const type = coupon.map(coupon => {
+      if (coupon.id === event.target.id) return coupon.type;
+    });
+
+    const mount = coupon.map(coupon => {
+      if (coupon.id === event.target.id) return coupon.value;
+    });
+
+    setResult(current => ({
+      ...current,
+      discount: COUPON,
+      coupon_id: event.target.id,
+      dicount_type: type.filter(value => value !== undefined),
+      discount_mount: mount.filter(value => value !== undefined),
+    }));
+
+    setIsMenu(false);
+  };
   // 쿠폰 리스트
   const couponList = coupon.map(coupon => {
     if (coupon.type === FIXED)
       return (
-        <Styled.DiscountCouponMenuContent key={coupon.id}>
-          <Styled.DiscountCouponValue>
+        <Styled.DiscountCouponMenuContent key={coupon.id} onClick={onUseCoupon}>
+          <Styled.DiscountCouponValue id={coupon.id}>
             {coupon.value} 원
           </Styled.DiscountCouponValue>
-          <Styled.DiscountCouponName>{coupon.name}</Styled.DiscountCouponName>
+          <Styled.DiscountCouponName id={coupon.id}>
+            {coupon.name}
+          </Styled.DiscountCouponName>
         </Styled.DiscountCouponMenuContent>
       );
     else if (coupon.type === RATED)
       return (
-        <Styled.DiscountCouponMenuContent key={coupon.id}>
-          <Styled.DiscountCouponValue>
+        <Styled.DiscountCouponMenuContent key={coupon.id} onClick={onUseCoupon}>
+          <Styled.DiscountCouponValue id={coupon.id}>
             {coupon.value} %
           </Styled.DiscountCouponValue>
-          <Styled.DiscountCouponName>{coupon.name}</Styled.DiscountCouponName>
+          <Styled.DiscountCouponName id={coupon.id}>
+            {coupon.name}
+          </Styled.DiscountCouponName>
         </Styled.DiscountCouponMenuContent>
       );
     return null;
