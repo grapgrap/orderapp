2943a31b382c130ed0570c33d34768af61b428f6
YouYou94 <ybh4115@gmail.com>
2022-09-13 15:15:11 +0900

    Discount, Constants.js 쿠폰 정률형 정액형 상수 추가

--- src/Constants.js
+++ src/Constants.js
@@ -4,6 +4,10 @@ export const CUSTOM = 'CUSTOM';
 export const DEFAULT = 'DEFAULT';
 
 // 할인
-export const NONE = 'NONE';
-export const COUPON = 'COUPON';
-export const POINT = 'POINT';
+export const NONE = '사용 안함';
+export const COUPON = '쿠폰 사용';
+export const POINT = '포인트 사용';
+
+// 쿠폰
+export const FIXED = 'FIXED';
+export const RATED = 'RATED';
