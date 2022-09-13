f8dea19d5f3dcbedf6cf5a2644ba68c2766e4329
YouYou94 <ybh4115@gmail.com>
2022-09-13 12:19:24 +0900

    Orderer, 피드백 2 : 기능 변수명 변경

--- src/Components/OrderPage/Orderer/Orderer.jsx
+++ src/Components/OrderPage/Orderer/Orderer.jsx
@@ -3,7 +3,7 @@ import * as Styled from './Styled.jsx';
 
 function Orderer({ user, result, setResult }) {
   // 상세 주소 변경
-  const onChange_Additional_address = event => {
+  const onChangeAdditionaladdress = event => {
     setResult(current => ({
       ...current,
       orderer: {
@@ -13,7 +13,7 @@ function Orderer({ user, result, setResult }) {
     }));
   };
   // 전화번호 변경
-  const onChange_Phone_Number = event => {
+  const onChangePhoneNumber = event => {
     setResult(current => ({
       ...current,
       orderer: {
@@ -35,13 +35,13 @@ function Orderer({ user, result, setResult }) {
         defaultValue={
           result.orderer.additional_address || user.additional_address
         }
-        onChange={onChange_Additional_address}
+        onChange={onChangeAdditionaladdress}
       />
       <Styled.UserInput
         name="phoneNum"
         type="text"
         defaultValue={result.orderer.phone_number || user.phone_number}
-        onChange={onChange_Phone_Number}
+        onChange={onChangePhoneNumber}
       />
     </CommonStyled.OrderPageSection>
   );
